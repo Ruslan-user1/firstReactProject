d42d003c55a37da530f8902615eb1e86f695323e
Ruslan <58146825+Ruslan-user1@users.noreply.github.com>
2020-04-13 14:00:19 +0300

    Загрузил проект в репозиторий

---
+++ src/components/Content.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+const Content = () => {
+    return <div className="content">
+    <img className="content__img-1" src="https://skazkisameli.ru/media/k2/items/cache/1b15decb97a7e442d17245363ff90a02_XL.jpg" alt=""/>
+    <img className="content__img-2" src="https://lapkins.ru/upload/iblock/130/1309222f033fb6928ea065578276ab44.jpg" alt=""/>
+  </div>
+}
+
+export default Content;
